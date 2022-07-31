8cde5c28f3d4d7527ca940516a203249f15867a1
Torres Nguyen <tnguyen@inmusicbrands.com>
2022-07-31 23:59:59 +1200

    Fix metadata values

--- src/func.rs
+++ src/func.rs
@@ -81,12 +81,14 @@ pub fn parsing_metadata(exif: Exif) -> Value {
         None => None,
     };
 
-    let created_time = match exif.get_field(Tag::DateTimeOriginal, In::PRIMARY) {
+    // TAG description: time that img_spec are created
+    let created_time = match exif.get_field(Tag::DateTimeDigitized, In::PRIMARY) {
         Some(created_time) => Some(created_time.display_value().with_unit(&exif).to_string()),
         None => None,
     };
 
-    let modified_time = match exif.get_field(Tag::DateTimeDigitized, In::PRIMARY) {
+    // TAG description: time that change file
+    let modified_time = match exif.get_field(Tag::DateTime, In::PRIMARY) {
         Some(modified_time) => Some(modified_time.display_value().with_unit(&exif).to_string()),
         None => None,
     };
@@ -96,17 +98,18 @@ pub fn parsing_metadata(exif: Exif) -> Value {
         None => None,
     };
 
-    let capture_time = match exif.get_field(Tag::DateTime, In::PRIMARY) {
+    // TAG description: capture time
+    let capture_time = match exif.get_field(Tag::DateTimeOriginal, In::PRIMARY) {
         Some(capture_time) => Some(capture_time.display_value().with_unit(&exif).to_string()),
         None => None,
     };
 
     serde_json::json!({
-        "camera_model": camera_model,
-        "serial_number": serial_number,
         "created_time": created_time,
         "modified_time": modified_time,
         "orientation": orientation,
-        "capture_time": capture_time
+        "capture_time": capture_time,
+        "camera_model": camera_model,
+        "serial_number": serial_number,
     })
 }
